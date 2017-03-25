717506a94dcda0716b3ce4bf38ada978cbd232f0
John <johnjmartiniv@gmail.com>
2017-03-25 17:36:50 -0400

    Missed file for re-base implementation.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAtnwNgh3qzpYFwurHb0Myg7k8ZLnRyTTA",
+  authDomain: "crowddj-be2bb.firebaseapp.com",
+  databaseURL: "https://crowddj-be2bb.firebaseio.com"
+}, 'crowddj-be2bb');
+
+export default base;
